49b48ad81e22a11788c2fa9eac2505803eb799ce
YU-HSIU <a1085524@mail.nuk.edu.tw>
2022-07-07 23:04:43 +0800

    完成基礎新增、刪除、修改 table 的 api

--- app.py
+++ app.py
@@ -1,20 +1,63 @@
+from unittest import result
 from flask import Flask, redirect,render_template, url_for,request,jsonify
+from jinja2 import Undefined
 import DB,json
 
 app = Flask(__name__)
 
 @app.route("/")             #切換 URL
-def hello():
-    content = DB.show_data('User')
-    print(json.dumps(content))
-    return jsonify(content)
-@app.route("/login",methods = ['POST','GET'])
-def login():
-    return render_template("login.html")
-@app.route("/index",methods = ['POST','GET'])
-def index():
-    return render_template("index.html")
+def Home():
+    return "Home Page"
 
+@app.route("/table/<name>")
+def table(name):
+    content = DB.show_data(name)
+    try:
+        return jsonify(content)
+    except TypeError as e:
+        return str(content)
+
+@app.route("/create/<name>")
+def insert(name):
+    data = {
+        "Content": "鼎元上台大",
+        "Note": "Turn On"
+    }
+    result = DB.insert_data(name,data)
+    if(result['success']):
+        return jsonify(result)
+    else:
+        return jsonify(result)
+
+@app.route("/delete/<name>")
+def delete(name):
+    pk = 3
+    result = DB.delete_data(name,pk)
+    if(result['success']):
+        return jsonify(result)
+    else:
+        return jsonify(result)
+
+@app.route("/deleteAll/<name>")
+def deleteAll(name):
+    result = DB.delete_all(name)
+    if(result['success']):
+        return jsonify(result)
+    else:
+        return jsonify(result)
+
+@app.route("/modify/<name>")
+def modify(name):
+    data = {
+        'Number': 12,
+        'Content':"好棒棒",
+        'Note': "眼睛業障重"
+    }
+    result = DB.modify_data(name,data)
+    if(result['success']):
+        return jsonify(result)
+    else:
+        return jsonify(result)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0',port='5000',debug=True)
